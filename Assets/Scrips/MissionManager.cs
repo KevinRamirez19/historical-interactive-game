dc97cf2c17606aceea09085884c4c8e6bf0bbc4d
OscarTorres24 <omanueltorres@ucundinamarca.edu.co>
2024-10-18 14:17:16 -0500

    Update game

--- Assets/Scrips/MissionManager.cs
+++ Assets/Scrips/MissionManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MissionManager : MonoBehaviour
@@ -13,33 +11,66 @@ public class MissionManager : MonoBehaviour
 
     void Start()
     {
-        StartMission(); // Comienza la misión
+        InitializeMission(); // Inicializa la misión
     }
 
-    public void StartMission()
+    private void InitializeMission()
+    {
+        // Desactiva todos los puntos de recogida y entrega
+        DeactivateAllPickupPoints();
+        DeactivateAllDeliveryPoints();
+
+        // Comienza la misión
+        StartMission();
+    }
+
+    private void StartMission()
     {
         currentPickupIndex = 0;
         currentDeliveryIndex = 0;
+
+        // Activa el primer punto de recogida
+        ActivatePickupPoint(currentPickupIndex);
         ShowMessage("Misión iniciada: Recoger el mensaje en " + pickupPoints[currentPickupIndex].name);
     }
 
-    public void Restart()
+    private void DeactivateAllPickupPoints()
     {
-        StartMission(); // Reinicia la misión
+        foreach (Transform pickupPoint in pickupPoints)
+        {
+            pickupPoint.gameObject.SetActive(false); // Desactiva cada punto de recogida
+        }
+    }
+
+    private void DeactivateAllDeliveryPoints()
+    {
+        foreach (Transform deliveryPoint in deliveryPoints)
+        {
+            deliveryPoint.gameObject.SetActive(false); // Desactiva cada punto de entrega
+        }
+    }
+
+    public bool CanPickup(int pickupIndex)
+    {
+        // Verifica si el índice de recogida coincide con el índice actual
+        return pickupIndex == currentPickupIndex;
     }
 
     public void OnMessagePickedUp()
     {
         // Al recoger el mensaje, avanzar al siguiente punto de entrega
         ShowMessage("Mensaje recogido en: " + pickupPoints[currentPickupIndex].name);
+        DeactivatePickupPoint(currentPickupIndex);
         currentPickupIndex++;
 
         if (currentPickupIndex < pickupPoints.Length)
         {
+            ActivateDeliveryPoint(currentDeliveryIndex);
             ShowMessage("Llevar el mensaje a " + deliveryPoints[currentDeliveryIndex].name);
         }
         else
         {
+            // Última entrega
             ShowMessage("Todos los mensajes recogidos. Llevar documento final a " + deliveryPoints[currentDeliveryIndex].name);
         }
     }
@@ -48,16 +79,62 @@ public class MissionManager : MonoBehaviour
     {
         // Al entregar el mensaje, avanzar al siguiente punto de recogida
         ShowMessage("Mensaje entregado en: " + deliveryPoints[currentDeliveryIndex].name);
+        DeactivateDeliveryPoint(currentDeliveryIndex);
         currentDeliveryIndex++;
 
         if (currentDeliveryIndex < deliveryPoints.Length)
         {
+            ActivatePickupPoint(currentPickupIndex);
             ShowMessage("Recoger el siguiente mensaje en " + pickupPoints[currentPickupIndex].name);
         }
         else
         {
             ShowMessage("Todas las entregas completadas. Misión finalizada.");
+            ShowFinalMessage(); // Muestra mensaje de fin del juego
         }
+    }
+
+    public void Restart()
+    {
+        // Reinicia la misión
+        InitializeMission();
+    }
+
+    private void ActivatePickupPoint(int index)
+    {
+        if (index < pickupPoints.Length)
+        {
+            pickupPoints[index].gameObject.SetActive(true);
+        }
+    }
+
+    private void DeactivatePickupPoint(int index)
+    {
+        if (index < pickupPoints.Length)
+        {
+            pickupPoints[index].gameObject.SetActive(false);
+        }
+    }
+
+    private void ActivateDeliveryPoint(int index)
+    {
+        if (index < deliveryPoints.Length)
+        {
+            deliveryPoints[index].gameObject.SetActive(true);
+        }
+    }
+
+    private void DeactivateDeliveryPoint(int index)
+    {
+        if (index < deliveryPoints.Length)
+        {
+            deliveryPoints[index].gameObject.SetActive(false);
+        }
+    }
+
+    private void ShowFinalMessage()
+    {
+        messageManager.ShowMessage("Juego Finalizado"); // Mensaje final
     }
 
     private void ShowMessage(string message)
--- Assets/Scrips/PickupPoint.cs
+++ Assets/Scrips/PickupPoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PickupPoint : MonoBehaviour
 {
+    public int currentPickupIndex; // Índice del punto de recogida
     private MissionManager missionManager;
 
     private void Start()
@@ -17,9 +18,12 @@ public class PickupPoint : MonoBehaviour
         // Verifica si el objeto que colisiona es el jugador
         if (other.CompareTag("Player"))
         {
-            // Llama al método para manejar la recogida de mensajes en el MissionManager
-            missionManager.OnMessagePickedUp();
-            Destroy(gameObject); // Opcional: Destruye el punto de recogida
+            if (missionManager != null && missionManager.CanPickup(currentPickupIndex)) // Verifica si el índice de recogida es válido
+            {
+                // Llama al método para manejar la recogida de mensajes en el MissionManager
+                missionManager.OnMessagePickedUp();
+                Destroy(gameObject); // Opcional: Destruye el punto de recogida
+            }
         }
     }
 }
